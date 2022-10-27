2948a250058dd47631fc246586859529a01b85f8
Terence Waters <terence@Terences-MacBook-Pro.local>
2022-10-27 14:39:28 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
 
 function App() {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '6399666c87mshd5fca0615f5dad7p15e8c6jsn554f4e03b481',
+      'X-RapidAPI-Host': 'youtube-search-results.p.rapidapi.com'
+    }
+  };
+
+  fetch('https://youtube-search-results.p.rapidapi.com/youtube-search/?q=justin%2Btimberlake', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+  async function searchYouTube(q: any) {
+    q = encodeURIComponent(q);
+    const response = await fetch('https://youtube-search-results.p.rapidapi.com/youtube-search/?q=' + q, {
+      'method': 'GET',
+      'headers': {
+        'x-rapidapi-host': 'youtube-search-results.p.rapidapi.com',
+        'x-rapidapi-key': '6399666c87mshd5fca0615f5dad7p15e8c6jsn554f4e03b481'
+      }
+    });
+    const body = await response.json();
+    console.log(body);
+    return body.items.filter((item: any) => item.type === 'video');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
